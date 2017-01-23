33ebbdf9c62725018da29446fc46f9cd001b1112
Daniel Rendon <daniel.rendonfc@gmail.com>
2017-01-23 15:11:38 -0600

    new fish can be created

--- catch-of-the-day/src/components/app.js
+++ catch-of-the-day/src/components/app.js
@@ -4,14 +4,35 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.addFish = this.addFish.bind(this);
+    // getinitialState
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish) {
+    // update our state
+    const fishes = {...this.state.fishes};
+    // add in our new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // set state
+    this.setState({ fishes });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header tagline="Fresh Seafood Market"/>
+          <Header tagline="Fresh Seafood Market" />
         </div>
-        <Order/>
-        <Inventory/>
+        <Order />
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
