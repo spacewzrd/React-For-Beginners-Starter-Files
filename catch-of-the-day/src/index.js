179a14c58bdf728f5a2ec3d8245194b343e4e902
Daniel Rendon <daniel.rendonfc@gmail.com>
2017-01-23 12:34:38 -0600

    added new components

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,8 +2,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './css/style.css'
-
+import App from './components/app';
 import StorePicker from './components/StorePicker';
 
 
-render(<StorePicker/>, document.querySelector('#main'));
+
+render(<App/>, document.querySelector('#main'));
