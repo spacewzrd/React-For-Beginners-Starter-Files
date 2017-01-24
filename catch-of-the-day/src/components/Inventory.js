d0fd3b7ba2cba2acb57a7aadb761b4b304c5281d
Daniel Rendon <daniel.rendonfc@gmail.com>
2017-01-24 12:15:39 -0600

    more state stuff

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -2,10 +2,40 @@ import React from 'react';
 import AddFishForm from './AddFishForm'
 
 class Inventory extends React.Component{
+  constructor(){
+    super();
+    this.renderInventory = this.renderInventory.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e, key) {
+    const fish = this.props.fishes[key];
+    console.log(fish);
+    const updatedFish = {...fish, [e.target.name]: e.target.value}
+    this.props.updateFish(key, updatedFish)
+  }
+
+  renderInventory(key){
+    const fish = this.props.fishes[key];
+    return(
+      <div className="fish-edit" key={key}>
+        <input type="text" name="name" placeholder="Fish Name" value={fish.name} onChange={(e) => this.handleChange(e, key)} />
+        <input type="text" name="price" placeholder="Fish Price" value={fish.price} onChange={(e) => this.handleChange(e, key)}/>
+        <select type="text" name="status" placeholder="Fish Status" value={fish.status} onChange={(e) => this.handleChange(e, key)}>
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea type="text" name="desc" placeholder="Fish Description" value={fish.desc} onChange={(e) => this.handleChange(e, key)}></textarea>
+        <input type="text" name="img" placeholder="Fish Image" value={fish.img} onChange={(e) => this.handleChange(e, key)}/>
+      </div>
+    )
+  }
+
   render() {
     return(
       <div>
         <p>Inventory</p>
+        {Object.keys(this.props.fishes).map(this.renderInventory)}
         <AddFishForm addFish={this.props.addFish}/>
         <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
--- catch-of-the-day/src/components/app.js
+++ catch-of-the-day/src/components/app.js
@@ -10,6 +10,7 @@ class App extends React.Component {
   constructor() {
     super();
     this.addFish = this.addFish.bind(this);
+    this.updateFish = this.updateFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
     this.addToOrder = this.addToOrder.bind(this);
     // getinitialState
@@ -52,6 +53,12 @@ class App extends React.Component {
     this.setState({ fishes });
   }
 
+  updateFish(key, updatedFish) {
+    const fishes = {...this.state.fishes};
+    fishes[key] = updatedFish
+    this.setState({fishes});
+  }
+
   loadSamples(){
     this.setState({
       fishes: sampleFishes
@@ -85,7 +92,12 @@ class App extends React.Component {
           order={this.state.order}
           params={this.props.params}
         />
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+      <Inventory
+        addFish={this.addFish}
+        loadSamples={this.loadSamples}
+        fishes={this.state.fishes}
+        updateFish={this.updateFish}
+      />
       </div>
     )
   }
